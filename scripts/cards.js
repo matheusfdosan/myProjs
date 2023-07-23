eb72afc7aa671cb816e7e0a05125ba431e32c587
matheusfdosan <matheusfaustinoe20@gmail.com>
2023-07-23 16:21:35 -0300

    now have all my repositories

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,44 +1,49 @@
-const API = fetch("https://api.github.com/users/matheusfdosan/repos")
+/**
+ * Pegar a URL
+ * Filtrar docs e markdown
+ * Adicionar cards
+ */
 
-API.then((response) => {
-  response
-    .json()
-    .then((data) => {
-      container = document.querySelector(".cards-grid")
+async function start() {
+  const username = "matheusfdosan"
+  const perPage = 100
+  let page = 1
 
-      data.forEach((repos) => {
-        if (
-          repos.topics.includes("docs") ||
-          repos.topics.includes("markdown")
-        ) {
-          return
-        }
+  const url = `https://api.github.com/users/${username}/repos?per_page=${perPage}&page=${page}`
+  const repos = await fetch(url).then((response) => response.json())
 
-        const HTMLModel = `
+  container = document.querySelector(".cards-grid")
+
+  repos.forEach((repos) => {
+    if (repos.topics.includes("docs") || repos.topics.includes("markdown")) {
+      return
+    }
+
+    const HTMLModel = `
       <div class="card" data-projects="${
         repos.name.replace(/-/g, " ") + " " + repos.description
       }">
-      <div class="img" style="background-image: url(https://raw.githubusercontent.com/matheusfdosan/${
-        repos.name
-      }/master/.github/preview.png);"></div>
-      <div class="text">
-      <h3>${repos.name.replace(/-/g, " ")}</h3>
-      <p>${repos.description}</p>
-      </div>
+        <div class="img" style="background-image: url(https://raw.githubusercontent.com/matheusfdosan/${
+          repos.name
+        }/master/.github/preview.png);"></div>
+        <div class="text">
+        <h3>${repos.name.replace(/-/g, " ")}</h3>
+        <p>${repos.description}</p>
+        </div>
       </div>`
 
-        container.insertAdjacentHTML("beforeend", HTMLModel)
-      })
-    })
-    .then(() => {
-      const homeContainer = document.querySelector(
-        ".some-projects > .cards-grid"
-      )
+    container.insertAdjacentHTML("beforeend", HTMLModel)
+  })
+}
 
-      const allCards = homeContainer.children
+start()
+  .then(() => {
+    const homeContainer = document.querySelector(".some-projects > .cards-grid")
 
-      for (let i = 6; i <= allCards.length; i++) {
-        allCards[i].classList.add("hidden")
-      }
-    })
-})
+    const allCards = homeContainer.children
+
+    for (let i = 6; i <= allCards.length; i++) {
+      allCards[i].classList.add("hidden")
+    }
+  })
+  .catch((err) => console.log("A little something wrong isn't right! " + err))
